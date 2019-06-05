22530c0b789aea75040fdcf82465c90e9665a977
EvanBradford <evan.bradford1@gmail.com>
2019-06-05 15:40:45 -0400

    Added basic implementations to subclasses

--- src/main/java/com/revature/banking/Customer.java
+++ src/main/java/com/revature/banking/Customer.java
@@ -6,5 +6,14 @@ public class Customer extends User{
 	 * 
 	 */
 	private static final long serialVersionUID = 2788622008317034552L;
-	
+
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Customer(String username, String password) {
+		super(username, password);
+		// TODO Auto-generated constructor stub
+	}
 }
--- src/main/java/com/revature/banking/SuperUser.java
+++ src/main/java/com/revature/banking/SuperUser.java
@@ -6,5 +6,15 @@ public class SuperUser extends User{
 	 * 
 	 */
 	private static final long serialVersionUID = -5319764797495658012L;
+
+	public SuperUser() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public SuperUser(String username, String password) {
+		super(username, password);
+		// TODO Auto-generated constructor stub
+	}
 	
 }
